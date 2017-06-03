611809f6640a6e39b47fd60dd9939c47e5ccbb11
Brian Fan <kuomu.fan@yahoo.com>
2017-06-03 11:12:40 -0700

    practice Displaying Dynamic Data

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{firstTask.description}}</h3>
   </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask = {
+    description: "Finish weekend Angular homework for Epicodus course"
+  }
 }
