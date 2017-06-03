51c90899459ca41124a4d375dbe43eac57f87d7b
Brian Fan <kuomu.fan@yahoo.com>
2017-06-03 12:43:28 -0700

    practice Event Bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{firstTask.description}}</h3>
+    <h3>{{currentFocus}}</h3>
+
+    <ul>
+      <li (click)='isDone(currentTask)' *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)='editTask()'>Edit!</button></li>
+    </ul>
+
   </div>
   `
 })
@@ -16,7 +21,23 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
+  tasks: Task[] = [
+  new Task('Finish weekend Angular homework for Epicodus course'),
+  new Task('Begin brainstorming possible JavaScript group projects'),
+  new Task('Add README file to last few Angular repos on GitHub')
+  ];
+
+  editTask() {
+    alert("You just requested to edit a Task!");
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert('This task is done!');
+    } else {
+      alert('This task is not done. Better get to work!');
+    }
+  }
 }
 
 export class Task {
