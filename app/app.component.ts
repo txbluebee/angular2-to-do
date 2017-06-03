df065b1621c26c264fd8fae80c73189613a7fe16
Brian Fan <kuomu.fan@yahoo.com>
2017-06-03 13:45:27 -0700

    practice Two-Way Data Binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,9 +8,21 @@ import { Component } from '@angular/core';
     <h3>{{currentFocus}}</h3>
 
     <ul>
-      <li [class]='priorityColor(currentTask)' (click)='isDone(currentTask)' *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)='editTask()'>Edit!</button></li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
-
+    <hr>
+    <div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+    </div>
   </div>
   `
 })
@@ -26,9 +38,10 @@ export class AppComponent {
   new Task('Begin brainstorming possible JavaScript group projects', 2),
   new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask: Task = this.tasks[0];
 
-  editTask() {
-    alert("You just requested to edit a Task!");
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
