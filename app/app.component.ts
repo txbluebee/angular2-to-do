91c0bfa46e8b6f74982e9c0af72e9beb00b51e49
Brian Fan <kuomu.fan@yahoo.com>
2017-06-03 14:09:42 -0700

    practice conditional directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
       <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
     <hr>
-    <div>
+    <div *ngIf="selectedTask">
       <h3>{{selectedTask.description}}</h3>
       <p>Task Complete? {{selectedTask.done}}</p>
       <h3>Edit Task</h3>
@@ -22,6 +22,7 @@ import { Component } from '@angular/core';
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <button (click)="finishedEditing()">Done</button>
     </div>
   </div>
   `
@@ -38,10 +39,14 @@ export class AppComponent {
   new Task('Begin brainstorming possible JavaScript group projects', 2),
   new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 
   isDone(clickedTask: Task) {
